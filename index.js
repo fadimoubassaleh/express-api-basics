c2d113c728aa05146fd4a31a05d854824d804beb
Jad Sarout <xananax@yelostudio.com>
2018-02-21 00:02:09 +0200

    step 6 - d - spaces

--- index.js
+++ index.js
@@ -6,53 +6,53 @@ const movies = [
   { title: 'Jaws', year: 1975, rating: 8 },
   { title: 'Avatar', year: 2009, rating: 7.8 },
   { title: 'Brazil', year: 1985, rating: 8 },
-  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }    
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }  
 ]
 
 app.get('/movies/add', ( req, res ) => {
-    res.send('ok')
+  res.send('ok')
 })
 
 app.get('/movies/get/by-date', ( req, res ) => {
-    const sortByDate = (a, b) => {
-        return a.year - b.year
-    }
-    const moviesSorted = movies.sort(sortByDate)
-    res.send({status:200, data:moviesSorted})
+  const sortByDate = (a, b) => {
+    return a.year - b.year
+  }
+  const moviesSorted = movies.sort(sortByDate)
+  res.send({status:200, data:moviesSorted})
 })
 
 app.get('/movies/get/by-rating', ( req, res ) => {
-    const sortByRating = (a, b) => {
-        return b.rating - a.rating
-    }
-    const moviesSorted = movies.sort(sortByRating)
-    res.send({status:200, data:moviesSorted})
+  const sortByRating = (a, b) => {
+    return b.rating - a.rating
+  }
+  const moviesSorted = movies.sort(sortByRating)
+  res.send({status:200, data:moviesSorted})
 })
 
 app.get('/movies/get/by-title', ( req, res ) => {
   const sortByTitle = (a, b) => {
-    const x = a.title.toLowerCase()
-    const y = b.title.toLowerCase()
-    return x<y ? -1 : x>y ? 1 : 0;
+  const x = a.title.toLowerCase()
+  const y = b.title.toLowerCase()
+  return x<y ? -1 : x>y ? 1 : 0;
   }
   const moviesSorted = movies.sort(sortByTitle)
   res.send({status:200, data:moviesSorted})
 })
 
 app.get('/movies/get', ( req, res ) => {
-    res.send({status:200, data:movies})
+  res.send({status:200, data:movies})
 })
 
 app.get('/movies/add', ( req, res ) => {
-    res.send('ok')
+  res.send('ok')
 })
 
 app.get('/movies/edit', ( req, res ) => {
-    res.send('ok')
+  res.send('ok')
 })
 
 app.get('/movies/delete', ( req, res ) => {
-    res.send('ok')
+  res.send('ok')
 })
 
 app.get('/test', (req,res) => res.send({status:200, message:"ok"}))
@@ -66,22 +66,22 @@ app.get('/time', (req, res) => {
 })
 
 app.get('/hello/:thing?', ( req, res ) => {
-    const thing = req.params.thing
-    const message = 'hello, '+thing
-    res.send({status:200, message})
+  const thing = req.params.thing
+  const message = 'hello, '+thing
+  res.send({status:200, message})
 })
 
 app.get('/search', (req, res)=>{
-    const query = req.query.s
-    if(query){
-        res.send({status:200, data:query})
-    }else{
-        res.status(500).send({status:500, error:true, message:"You have to provide a search"})
-    }
+  const query = req.query.s
+  if(query){
+    res.send({status:200, data:query})
+  }else{
+    res.status(500).send({status:500, error:true, message:"You have to provide a search"})
+  }
 })
 
 app.get('/',(req,res)=>{
-    res.send('batata')
+  res.send('batata')
 })
 
 app.listen( port, () => {
