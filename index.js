5b00573ab82946645bb9fa554a766f34e0582f4e
Jad Sarout <xananax@yelostudio.com>
2018-02-20 23:44:29 +0200

    step 3 - b

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/test', (req,res)=>res.send({status:200, message:"ok"}))
+app.get('/test', (req,res) => res.send({status:200, message:"ok"}))
+
+app.get('/time', (req, res) => { 
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const message = hours+":"+minutes
+  res.send({status:200, message})
+})
 
 app.get('/',(req,res)=>{
     res.send('batata')
