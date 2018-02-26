5db8810f497841297035b92452f999fd55c25056
fadi <fadi mabsaleh>
2018-02-26 17:14:27 +0200

    step 3.0

--- express/index.js
+++ express/index.js
@@ -7,6 +7,20 @@ app.get((""), (req, res)=>{
     res.send('Batata')
 })
 
+app.get(("/test"), (req, res)=>{
+    res.send('{status:200, message:"ok"}')
+})
+
+app.get(("/time"), (req, res)=>{
+    console.log(Date.now())
+    function nowTime() {
+        const test = new Date();
+        const result = test.getHours() + `:` + test.getMinutes()
+        return result
+    }
+    res.send(`{status:200, message:`+nowTime()+`}`)
+})
+
 app.listen(port,(err)=>{
     if(err){
         console.log('something bad happened', err)
