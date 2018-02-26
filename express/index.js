62882eed5f0a48f0d4941c8f2cf77ffd642f903e
fadi <fadi mabsaleh>
2018-02-26 19:19:31 +0200

    step 4

--- express/index.js
+++ express/index.js
@@ -18,9 +18,38 @@ app.get(("/time"), (req, res)=>{
         const result = test.getHours() + `:` + test.getMinutes()
         return result
     }
-    res.send(`{status:200, message:`+nowTime()+`}`)
+    res.status(200)
+    res.send(nowTime())
 })
 
+app.get(("/hello/:id"), (req, res)=>{
+    res.status(200)
+    res.write(req.params.id)
+    res.end()
+})
+
+app.get(('/search'), (req, res)=>{
+    // const myURL = new URL('https://example.org/?abc=123');
+    // console.log(myURL.searchParams.get('abc'));
+    // console.log('hi')
+    // console.log(req.searchParams.get())
+    // res.send(req.searchParams.get())
+    console.log(req.query.s)
+    // res.send(req.query.s)
+    if (!req.query.s){
+        res.status(500);
+        res.write('you have to provide a search')
+        res.err = true
+        res.end()
+    }else{
+        res.status(200)
+        res.write('ok')
+        res.data = req.query.s
+        res.end()
+    }
+    
+})
+// app.get((''), (req, res)=>{})
 app.listen(port,(err)=>{
     if(err){
         console.log('something bad happened', err)
